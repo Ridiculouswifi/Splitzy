ceaf957cd056ffd595fd4411a71c18c66cfd84af
Ridiculouswifi <thtan02@gmail.com>
2024-12-06 12:01:36 +0800

    Add base file for trip details page

--- app/index.tsx
+++ app/index.tsx
@@ -4,11 +4,13 @@ import { Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Home from "./screens/home";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Trip from "./screens/trip";
 
 const Stack = createNativeStackNavigator<ParamsList>();
 
 export type ParamsList = {
     Home: {name: string}
+    Trip: {name: string}
 }
 
 export default function Index() {
@@ -16,6 +18,7 @@ export default function Index() {
         <SafeAreaProvider>
             <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
                 <Stack.Screen name="Home" component={Home}/>
+                <Stack.Screen name="Trip" component={Trip}/>
             </Stack.Navigator>
         </SafeAreaProvider>
     );
