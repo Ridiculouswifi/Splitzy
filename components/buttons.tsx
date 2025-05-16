b5fb647873b3e1212fa35821c39690a6d57e3bd2
Ridiculouswifi <thtan02@gmail.com>
2025-05-16 21:38:48 +0800

    Edit button animations

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 interface genericButtonProps {
     height: number;
@@ -32,7 +32,7 @@ const genericButtonStyles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 1,
         elevation: 1,
-        transform: [{translateY: 1.3}],
+        transform: [{translateY: 1}],
     },
     text: {
         fontWeight: 'bold',
@@ -43,14 +43,16 @@ export function GenericButton({ action, height, width, colour, text, fontsize, t
     const [pressed, setPressed] = useState<boolean>(false);
 
     return (
-        <Pressable 
-            style={[pressed ? genericButtonStyles.buttonPressed : genericButtonStyles.button, 
-                pressed ? {height: height, width: width/1.01, backgroundColor: colour} : {height: height, width: width, backgroundColor: colour}]}
-            onPress={action}
-            onPressIn={() => pressIn(setPressed)}
-            onPressOut={() => pressOut(setPressed)}>
-            <Text style={[genericButtonStyles.text, {color: textColour, fontSize: fontsize}]}>{text}</Text>
-        </Pressable>
+        <View style={{height: height, width: width, alignItems: 'center', justifyContent: 'center'}}>
+            <Pressable 
+                style={[pressed ? genericButtonStyles.buttonPressed : genericButtonStyles.button, 
+                    pressed ? {height: height/1.01, width: width/1.01, backgroundColor: darkenHexColor(colour, 0.95)} : {height: height, width: width, backgroundColor: colour}]}
+                onPress={action}
+                onPressIn={() => pressIn(setPressed)}
+                onPressOut={() => pressOut(setPressed)}>
+                <Text style={[genericButtonStyles.text, {color: textColour, fontSize: pressed ? fontsize/1.01 : fontsize}]}>{text}</Text>
+            </Pressable>
+        </View>
     )
 }
 
@@ -60,4 +62,24 @@ function pressIn(setPressed: (variable: boolean) => void) {
 
 function pressOut(setPressed: (variable: boolean) => void) {
     setPressed(false);
+}
+
+function hexToRgb(hex: string): number[] {
+  const bigint = parseInt(hex.replace('#', ''), 16);
+  return [(bigint >> 16) & 255, (bigint >> 8) & 255, bigint & 255];
+}
+
+function rgbToHex(r: number, g: number, b: number): string {
+  return (
+    '#' +
+    [r, g, b]
+      .map(c => c.toString(16).padStart(2, '0'))
+      .join('')
+  );
+}
+
+// Factor is the percentage of lightness: eg. 0.9 is 90% of brightness left
+function darkenHexColor(hex: string, factor: number): string {
+  const [r, g, b] = hexToRgb(hex).map(c => Math.round(c * factor));
+  return rgbToHex(r, g, b);
 }
